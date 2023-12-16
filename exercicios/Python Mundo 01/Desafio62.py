3d6982b7be166eb4fb7e5a9e1f4fca88cf9d9aee
Nathaly-v <nathalydc2023@gmail.com>
2023-12-16 15:49:15 -0300

    Novos Desafios

--- exercicios/Python Mundo 01/Desafio62.py
+++ exercicios/Python Mundo 01/Desafio62.py
@@ -1,1 +1,29 @@
 # Exercício Python 62: Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
+
+print(' Gerador de PA')
+print('-='*30)
+
+termo = int(input( 'Primeiro termo : '))
+razao =  int(input( 'Razão DA PA : '))
+cont = 1
+contTotal=0
+maisTermo = 1  
+while cont <= 10:
+    print(f'{termo}',end = ' ↦ ')
+    termo = termo + razao
+    cont = cont + 1          
+    contTotal = contTotal + 1
+print('PAUSA \n')
+while maisTermo != 0:
+    maisTermo= int(input(' Quantos termos você quer mostrar a mais ? '))
+    cont = 1
+    while cont <= maisTermo:
+        print(f'{termo}',end = ' ↦ ')
+        termo = termo + razao
+        cont = cont + 1
+        contTotal = contTotal + 1
+    if maisTermo != 0:
+        print('PAUSA')
+print (f'Progressão finalizada com {contTotal} termos mostrados')
+      
+    
